6dfe75db3868f61064cecb461d4ae147eb7a1fd4
KirzhnerMark <markeybass@gmail.com>
2023-07-29 19:56:13 +0300

    made proper changes for Prometheus monitoring

--- server/app.py
+++ server/app.py
@@ -140,4 +140,4 @@ def delete_todo(id):
     return todo_schema.jsonify(todo)
 
 if __name__=='__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
